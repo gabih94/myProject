08fcedd8fb688aa9d2026f0b3f27d4f7888e0877
gabih94 <gabimromualdo94@gmail.com>
2024-11-13 01:22:02 -0300

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: "products#index"
+
+#rake routes  get 'products/:id' => 'products#show', as: 'produto'
+
+
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
