50ce954cb1d102f58d336a193b10180e5f382260
gabih94 <gabimromualdo94@gmail.com>
2025-01-05 22:28:02 -0300

    add gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: "products#index"
 
 #rake routes  get 'products/:id' => 'products#show', as: 'produto'
